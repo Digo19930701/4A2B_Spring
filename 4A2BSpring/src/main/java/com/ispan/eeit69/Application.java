42f1f73bc1c3248d702ac2b3ff7a51017c40349c
Digo19930701 <yoyo820701@gmail.com>
2023-09-28 16:54:53 +0800

    feat:23030928

---
+++ 4A2BSpring/src/main/java/com/ispan/eeit69/Application.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
